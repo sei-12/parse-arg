207db9ed93cf936dd37c39815cdce60955e2d0d8
おもちくん <sei.t11@icloud.com>
2023-03-07 09:54:44 +0900

    del get arg

--- main.c
+++ main.c
@@ -2,21 +2,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-int get_arg(char* opt,char* buf,int argc, char* argv[]){
-    for(int i = 0; i<argc ; i++){
-        if(strcmp(argv[i],opt) == 0){
-            if(i + 1 == argc){
-                printf("引数が足りません");
-                continue;
-            }
-            strcpy(buf,argv[i + 1]);
-            return 0;
-        }
-    }
-
-    return -1;
-}
-
 
 // 可変個の引数に対応していない
 struct Argment{
