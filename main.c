dabf3bf438973c9a80021b2d5bf7a0b07a6fd8f5
おもちくん <sei.t11@icloud.com>
2023-03-08 04:43:25 +0900

    update main.c

--- main.c
+++ main.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
-
-// 可変個の引数に対応していない
-struct Argment{
-    bool exists;
-    char value[128];
-};
-
-void parse_arg(struct Argment* arg, char key[], int argc, char* argv[]){
-    arg->exists = false;
-
-    for(int i = 0; i < argc ; i++){
-        if(strcmp(argv[i],key) != 0) continue;
-        arg->exists = true;
-
-        if( i + 1 == argc){
-            break;
-        }
-
-        if(strncmp(argv[i + 1],"-",1) == 0 || strncmp(argv[i + 1],"--",2) == 0 ){
-            break;
-        }
-
-        strcpy(arg->value,argv[i + 1]);
-        return;
-    }
-    strcpy(arg->value,"");
-    return;
-
-}
+#include "arg.h"
 
 int main(int argc, char* argv[]){
     struct Argment arg;  parse_arg(&arg,"-p",argc,argv);
