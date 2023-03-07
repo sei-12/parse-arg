7594f6c6e332e0fad06c7857c6fb6a43ee02288a
おもちくん <sei.t11@icloud.com>
2023-03-08 04:17:08 +0900

    update main

--- main.c
+++ main.c
@@ -33,9 +33,16 @@ void parse_arg(struct Argment* arg, char key[], int argc, char* argv[]){
 }
 
 int main(int argc, char* argv[]){
-    struct Argment arg; parse_arg(&arg,"-p",argc,argv);
+    struct Argment arg;  parse_arg(&arg,"-p",argc,argv);
+    struct Argment arg2; parse_arg(&arg2,"-a",argc,argv);
 
-    printf("print: %s\n",arg.value);
+    if(arg.exists){
+        printf("print: %s\n",arg.value);
+    }
+
+    if(arg2.exists){
+        printf("arg2\n");
+    }
 
     return 0;
 }
