48eebd658ca3f30842b7cf572eaa1b53e5186566
おもちくん <sei.t11@icloud.com>
2023-03-07 09:54:31 +0900

    add parse arg

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int get_arg(char* opt,char* buf,int argc, char* argv[]){
     for(int i = 0; i<argc ; i++){
@@ -13,6 +14,35 @@ int get_arg(char* opt,char* buf,int argc, char* argv[]){
         }
     }
 
+    return -1;
+}
+
+
+// 可変個の引数に対応していない
+struct Argment{
+    char key[64];
+    bool exists;
+    char value[128];
+};
+
+int parse_arg(struct Argment* arg, int argc, char* argv[]){
+    arg->exists = false;
+    
+    for(int i = 0; i < argc ; i++){
+        if(strcmp(argv[i],arg->key) != 0) continue;
+        arg->exists = true;
+
+        if( i + 1 == argc){
+            return -1;
+        }
+
+        if(strncmp(argv[i + 1],"-",1) == 0 || strncmp(argv[i + 1],"--",2) == 0 ){
+            return -1;
+        }
+
+        strcpy(arg->value,argv[i + 1]);
+        return 0;
+    }
     return -1;
 }
 
